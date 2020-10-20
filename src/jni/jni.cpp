b8732d0e2e35c1843786f33356b7182e6a079366
Michael Pozhidaev <msp@luwrain.org>
2020-10-20 12:01:18 +0700

    jni: Transforming java bytes to std::string

--- src/jni/jni.cpp
+++ src/jni/jni.cpp
@@ -1,9 +1,33 @@
 
+#include<string>
+#include<iostream>
 
 #include"jni.h"
 #include"org_luwrain_runyms_Runyms.h"
 
-int main()
+#include "../aot/LemmatizerLib/MorphologyHolder.h"
+#include"../aot/LemmatizerLib/Lemmatizers.h"
+#include"../aot/LemmatizerLib/Morphan.h"
+
+JNIEXPORT void JNICALL Java_org_luwrain_runyms_Runyms_init
+  (JNIEnv* env, jobject obj)
 {
-  return 0;
+    CMorphologyHolder Holder;
+    if (!Holder.LoadLemmatizer(morphRussian))
+      {
+        std::cerr << "Cannot load morphology\n";
+    }
 }
+
+JNIEXPORT jbyteArray JNICALL Java_org_luwrain_runyms_Runyms_lemmatizeImpl
+  (JNIEnv* env, jobject obj, jbyteArray word)
+{
+          const jsize len = env->GetArrayLength(word);
+	          const jbyte* bytes = env->GetByteArrayElements(word, NULL); 
+	  std::string w;
+	  w.reserve(len);
+	  for(jsize i = 0;i < len;i++)
+	    w += bytes[i];
+	  std::cout << w << std::endl;
+}
+
--- src/jni/org/luwrain/runyms/Runyms.java
+++ src/jni/org/luwrain/runyms/Runyms.java
@@ -3,5 +3,14 @@ package org.luwrain.runyms;
 
 public class Runyms
 {
-    public native void init();
+    private native void initImpl();
+    private native byte[] lemmatizeImpl(byte[] word);
+
+    static public void main(String[] args) throws Exception
+    {
+	System.loadLibrary("runyms_jni");
+	final Runyms r = new Runyms();
+	final String text = "Это пробный текст";
+	r.lemmatizeImpl(text.getBytes("UTF-8"));
+    }
 }
