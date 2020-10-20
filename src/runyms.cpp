9689426df083c15a5ae22130e7226fe5a526e4d4
Michael Pozhidaev <msp@luwrain.org>
2020-10-20 08:40:51 +0700

    The lemmatizer works! (it seems to me)

--- src/runyms.cpp
+++ src/runyms.cpp
@@ -1,8 +1,22 @@
 
+#include<string>
+#include<vector>
+
+#include "aot/LemmatizerLib/MorphologyHolder.h"
 #include"aot/LemmatizerLib/Lemmatizers.h"
+#include"aot/LemmatizerLib/Morphan.h"
 
 int main()
 {
-  CLemmatizerRussian l;
-  return 0;
+    CMorphologyHolder Holder;
+    if (!Holder.LoadLemmatizer(morphRussian))
+      {
+        std::cerr << "Cannot load morphology\n";
+        return 1;
+    }
+    std::cout << "Morphology loaded" << std::endl;
+    std::string s = convert_from_utf8("века", morphRussian);
+    std::string result = LemmatizeJson(s.c_str(), &Holder, /*printForms*/true, true, true);
+    std::cout << result << std::endl;
+    return 0;
 }
